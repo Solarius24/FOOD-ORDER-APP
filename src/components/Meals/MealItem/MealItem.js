328abe06ea2cf57e36a544e439a109144d948752
Pawel Karkocki <pawel.karkocki@gmail.com>
2022-11-12 18:57:34 +0000

    fetch data from firebase and post added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,19 +1,23 @@
-import React, {useContext} from "react";
-import classes from "./MealItem.module.css";
+import { useContext } from "react";
+
 import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
 import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
-  const cartCtx = useContext(CartContext)
-  const price = `$${props.price.toFixed(2)}`;
-  const addItemToCartHandler = amount => {
+  const cartCtx = useContext(CartContext);
+
+  const price = `$ ${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
-      id:props.id,
-      name:props.name,
+      id: props.id,
+      name: props.name,
       amount: amount,
-      price: props.price
-    })
-  }
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -22,7 +26,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-      <MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/>
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
